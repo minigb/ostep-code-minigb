08a400a081cea34cd0f855c5bda2f7f1e400f1a4
Minhee Lee <minigimbob@gmail.com>
2023-09-08 03:12:48 +0000

    Experiment about background and foreground programs

--- intro/cpu.c
+++ intro/cpu.c
@@ -10,7 +10,7 @@ int main(int argc, char *argv[])
 	exit(1);
     }
     char *str = argv[1];
-    printf("%s, %ld", str, strlen(str));
+    // printf("%s, %ld", str, strlen(str));
     /*
     TODO(minigb): It's interesting that the input argument "A", like
     ./cpu "A"
@@ -31,6 +31,25 @@ int main(int argc, char *argv[])
 
     while (1) {
 	printf("%s\n", str);
+    if (str[0] == 'A') {
+        break;
+        /*
+        Add this and run
+        ./cpu A & ./cpu B & ./cpu C ; fg
+        prints
+        A
+        B
+        C
+        B
+        C
+        ...
+        and then interrupt with Ctrl-C, then only B is printed
+        B
+        B
+        ...
+        because the foreground program 'C' is interrupted
+        */
+    }
 	Spin(1);
     }
     return 0;
